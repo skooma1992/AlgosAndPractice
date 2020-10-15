b21860f9eac8cfcb04be331d47d7abe41dbcc853
skooma1992 <upsonjake@gmail.com>
2020-10-15 19:15:30 -0400

    ok confused

---
+++ Pathfinder/index.js
@@ -0,0 +1,26 @@
+let cols = 5;
+let rows = 5;
+let grid = new Array(cols)
+
+function Spot(){
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+}
+
+function setup() {
+  createCanvas(400, 400);
+  console.log('A*')
+}
+
+for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; i++){
+  grid[i][j] = new Spot()
+}
+}
+
+console.log(grid)
+ 
+function draw(){
+  background(0);
+}
